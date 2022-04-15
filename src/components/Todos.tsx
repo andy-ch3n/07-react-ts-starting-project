1028004920c5fa1c5c6fb878a22c71e3743fc936
Andy Chen <adch3n@gmail.com>
2022-04-14 23:06:50 -0700

    typescript practice

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -6,15 +6,13 @@ import { TodosContext } from '../store/todos-context'
 
 const Todos: React.FC<{items: Todo[]; onRemoveTodo: (id: string) => void }> = (props) => {
 
- const todosCtx =  useContext(TodosContext);
+ const todosCtx = useContext(TodosContext);
 
 
   return (
     <ul className={classes.todos}>
      {todosCtx.items.map((item) => (
-
        <TodoItem key={item.id} text={item.text} onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)}/>
-
        ))}
     </ul>
   )
